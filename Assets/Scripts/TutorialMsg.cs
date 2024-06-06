51a8b15173c151d36696757903572f0099e3d758
subinirane <129473417+subinirane@users.noreply.github.com>
2024-06-06 17:29:45 +0900

    feat: 튜토리얼 제작

---
+++ Assets/Scripts/TutorialMsg.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TutorialMsg: MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public List<string> _dialog = new List<string>();
+    public int _dialogNum;
+    public GameObject img;
+
+    public int CharPerSeconds;
+    private string targetmsg;
+    private int index;
+    private float interval;
+    private bool isEffect;
+
+    //텍스트 이펙트없이 다 보이게 해주는 기능을 위한 변수
+    private bool msgCheck;
+    private int _dialogNum2;
+
+    private Fade fadeScript;
+    public TutorialManager tm;
+    public int textNum;
+    
+
+    void Start()
+    {
+        fadeScript = GetComponent<Fade>();
+        _dialogNum = 0;
+        _dialogNum2 = 0;
+        msgCheck = true;
+        NextDialog();
+        interval = 1.0f / CharPerSeconds;
+    }
+
+    void OnEnable()
+    {
+        _dialogNum = 0;
+        _dialogNum2 = 0;
+        msgCheck = true;
+        NextDialog();
+        interval = 1.0f / CharPerSeconds;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            NextDialog();
+    }
+
+    public void NextDialog()
+    {
+        if (!msgCheck)
+        {
+            Setmsg2(_dialog[_dialogNum2]);
+            msgCheck = true;
+            _dialogNum2++;
+        }
+        else
+        {
+            if (_dialogNum < _dialog.Count)
+            {
+                Setmsg(_dialog[_dialogNum]);
+                _dialogNum++;
+                msgCheck = false;
+            }
+            else
+            {
+                if (textNum == 0 || textNum == 5)
+                    fadeScript.LoadFade();
+                else
+                {
+                    tm.Change(textNum);
+                }
+
+               // if (SceneManager.GetActiveScene().buildIndex == 1)
+               //     SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    public void Setmsg(string msg)
+    {
+        if (isEffect)
+        {
+            CancelInvoke();
+            EffectEnd();
+        }
+        targetmsg = msg;
+        EffectStart();
+    }
+
+    void Setmsg2(string msg)
+    {
+        if (isEffect)
+        {
+            CancelInvoke();
+            EffectEnd();
+        }
+        text.text = msg;
+    }
+
+    void EffectStart()
+    {
+        img.SetActive(false);
+        text.text = "";
+        index = 0;
+        isEffect = true;
+        Invoke("Effecting", interval);
+    }
+
+    void Effecting()
+    {
+        if (text.text == targetmsg)
+        {
+            EffectEnd();
+            return;
+        }
+
+        text.text += targetmsg[index];
+        index++;
+
+        Invoke("Effecting", interval);
+    }
+
+    void EffectEnd()
+    {
+        isEffect = false;
+        img.SetActive(true);
+    }
+}
